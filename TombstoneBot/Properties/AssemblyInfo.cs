67018687f3de07518241ca37513603273aeadfed
Dexter <34259253+triple7inc@users.noreply.github.com>
2025-04-19 04:38:46 +0300

    v0.7.8

--- TombstoneBot/Properties/AssemblyInfo.cs
+++ TombstoneBot/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TombstoneBot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("triple7inc")]
 [assembly: AssemblyProduct("TombstoneBot")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © triple7inc 2025")]
+[assembly: AssemblyTrademark("triple7inc")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
